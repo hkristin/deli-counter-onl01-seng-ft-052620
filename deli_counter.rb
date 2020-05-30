5441ffd5a3bd294c72aa9af6f829e47012157d78
Kristin Howard <hkristin@andrew.cmu.edu>
2020-05-30 23:15:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,10 @@
 katz_deli =[]
 
-
-
 def line(katz_deli)
   
   katz_deli.map!.with_index(1) do |name, index|
     "#{index}. #{name}"
   end
-  
   
   if katz_deli.length > 0
     puts "The line is currently: #{katz_deli.join(" ")}"
