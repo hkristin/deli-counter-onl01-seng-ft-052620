e8d95d99eeece17610cd869148fa3b259e96184e
Kristin Howard <hkristin@andrew.cmu.edu>
2020-05-30 23:13:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+#katz_deli =[]
+
+require 'pry'
+
+def line(katz_deli)
+  
+  katz_deli.map!.with_index(1) do |name, index|
+    "#{index}. #{name}"
+  end
+  
+  
+  if katz_deli.length > 0
+    puts "The line is currently: #{katz_deli.join(" ")}"
+  else 
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, name)
+  
+end
+
+def now_serving
+  puts
+  
+end
