b5249a246d0cf7ccf9b3c8f329d3fb4761d3d49f
Kristin Howard <hkristin@andrew.cmu.edu>
2020-05-30 23:22:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 
 def take_a_number(katz_deli, name)
   if katz_deli.length > 0
-  puts "Welcome, #{name}. You are number #{index +1} in line."
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
 def now_serving
